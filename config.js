ae9cc2b0c23125faa303fd79a6216db1df8913b8
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2020-06-21 00:45:23 -0300

    Configuracion servidor smtp para envio de mails

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB || 'mongodb://localhost:27017/dbgeneral',
-  SECRET_TOKEN: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+  SECRET_TOKEN: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  USER_EMAIL: process.env.USER_EMAIL_BONAPP,
+  USER_PASSWORD: process.env.USER_PASSWORD_BONAPP
 }
--- shared/helpers/emailSender.js
+++ shared/helpers/emailSender.js
@@ -1,5 +1,6 @@
 'use strict'
 const nodemailer = require("nodemailer");
+const config = require('../../config');
 
 /**
  * Función para el envío de mails.
@@ -12,15 +13,13 @@ async function sendEmail(email) {
     let testAccount = await nodemailer.createTestAccount();
 
     // create reusable transporter object using the default SMTP transport
-    //TODO: cambiar por un servidor smtp real, o ver algún paquete de npm que pueda llegar a servir
-    //como transport. Ver Sendgrid (https://sendgrid.com/)
     let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      host: "smtp.mail.bonapp.com.ar",
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
+        user: config.USER_EMAIL,
+        pass: config.USER_PASSWORD, 
       },
     });
 
